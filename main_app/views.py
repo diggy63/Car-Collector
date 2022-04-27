ffc3c7ebf71369bb0e7dd5f93e9d0d5571985d5e
diggy63 <daniel.kanczugowski@gmail.com>
2022-04-27 16:07:30 -0700

    finished lab

--- main_app/views.py
+++ main_app/views.py
@@ -30,8 +30,13 @@ def cars_index(request):
 
 def cars_detail(request, car_id):
     car = Car.objects.get(id=car_id)
+    services_not_have = Service.objects.exclude(id__in = car.services.all().values_list('id'))
     oil_form = OilchangeForm()
-    return render(request, 'cars/detail.html', {'car' : car, 'oil_form': oil_form})
+    return render(request, 'cars/detail.html', {
+        'car' : car, 
+        'oil_form': oil_form,
+        'services': services_not_have
+    })
 
 def add_oilchange(request, car_id):
     print(request.POST['miles'])
@@ -50,7 +55,7 @@ def add_oilchange(request, car_id):
 def cars_new(request):
     return render(request, 'cars/new.html')
 
-def assoc_service(request, cat_id, service_id):
+def assoc_service(request, car_id, service_id):
   # Note that you can pass a toy's id instead of the whole object
   Car.objects.get(id=car_id).services.add(service_id)
   return redirect('detail', car_id=car_id)
